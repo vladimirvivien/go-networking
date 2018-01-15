1981de72e47ab6c14232b73454c8544b46303d4f
Vladimir Vivien <vladimir.vivien@gmail.com>
2018-01-14 21:58:05 -0500

    TCP server with custom protocol

--- currency/server2/server2.go
+++ currency/server2/server2.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"io"
@@ -30,8 +31,8 @@ var currencies = curr.Load("../data.csv")
 // Focus:
 // This version of the currency server focuses on implementing a streaming
 // strategy when receiving data from client to avoid dropping data when the
-// request is larger than the internal buffer. This relies on the fact that
-// net.Conn implements io.Reader which allows the code to stream data.
+// request is larger than the internal buffer. This version uses the bufio
+// package to use buffered readers to stream from net.Conn.
 //
 // Testing:
 // Netcat or telnet can be used to test this server by connecting and
@@ -87,51 +88,29 @@ func handleConnection(conn net.Conn) {
 		}
 	}()
 
-	if _, err := conn.Write([]byte("Connected...\nUsage: GET <currency, country, or code>\n")); err != nil {
+	if _, err := fmt.Fprint(conn, "Connected...\nUsage: GET <currency, country, or code>\n"); err != nil {
 		log.Println("error writing:", err)
 		return
 	}
 
-	// appendBytes is a func that simulates end-of-File marker error.
-	// Since we will using streaming IO on top of a streaming protocol, there may never be
-	// an actual EOF marker.  So this function simulates and io.EOF using char '\n'.
-	appendBytes := func(dest, src []byte) ([]byte, error) {
-		for _, b := range src {
-			if b == '\n' {
-				return dest, io.EOF
-			}
-			dest = append(dest, b)
-		}
-		return dest, nil
-	}
+	// buffered reader to stream data using 4-byte chunks until ('\n\')
+	// The chunks are kept small to demonstrate streaming using io.Reader.
+	reader := bufio.NewReaderSize(conn, 4)
 
-	// loop to stay connected with client until client breaks connection
+	// command-loop
 	for {
-		// buffer for client command
-		var cmdLine []byte
-
-		// stream data using 4-byte chunks until io.EOF ('\n\')
-		// The chunks are kept small to demonstrate streaming using io.Reader.
-		for {
-			chunk := make([]byte, 4)
-			n, err := conn.Read(chunk)
-			if err != nil {
-				// io.EOF may never happen since this is a stream
-				if err == io.EOF {
-					cmdLine, _ = appendBytes(cmdLine, chunk[:n]) // read remaining bytes
-					break
-				}
+		cmdLine, err := reader.ReadString('\n')
+		if err != nil {
+			if err != io.EOF {
 				log.Println("connection read error:", err)
 				return
 			}
-			if cmdLine, err = appendBytes(cmdLine, chunk[:n]); err == io.EOF {
-				break
-			}
 		}
+		reader.Reset(conn) //cleans buffer
 
-		cmd, param := parseCommand(string(cmdLine))
+		cmd, param := parseCommand(cmdLine)
 		if cmd == "" {
-			if _, err := conn.Write([]byte("Invalid command\n")); err != nil {
+			if _, err := fmt.Fprint(conn, "Invalid command\n"); err != nil {
 				log.Println("failed to write:", err)
 				return
 			}
@@ -143,19 +122,19 @@ func handleConnection(conn net.Conn) {
 		case "GET":
 			result := curr.Find(currencies, param)
 			if len(result) == 0 {
-				if _, err := conn.Write([]byte("Nothing found\n")); err != nil {
+				if _, err := fmt.Fprint(conn, "Nothing found\n"); err != nil {
 					log.Println("failed to write:", err)
 				}
 				continue
 			}
 			// send each currency info as a line to the client wiht fmt.Fprintf()
 			for _, cur := range result {
-				_, err := conn.Write([]byte(
-					fmt.Sprintf(
-						"%s %s %s %s\n",
-						cur.Name, cur.Code, cur.Number, cur.Country,
-					),
-				))
+				_, err := fmt.Fprintf(
+					conn,
+					"%s %s %s %s\n",
+					cur.Name, cur.Code, cur.Number, cur.Country,
+				)
+
 				if err != nil {
 					log.Println("failed to write response:", err)
 					return
@@ -163,7 +142,7 @@ func handleConnection(conn net.Conn) {
 			}
 
 		default:
-			if _, err := conn.Write([]byte("Invalid command\n")); err != nil {
+			if _, err := fmt.Fprintf(conn, "Invalid command\n"); err != nil {
 				log.Println("failed to write:", err)
 				return
 			}
